d700bd500ba275da6532c1995efe62955d3a3a9b
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-11-21 17:01:35 +0100

    Modifier / supprimer mes adresses

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -47,9 +47,47 @@ class AccountAddressController extends AbstractController
             return $this->redirectToRoute('account_address');
         }
 
-        return $this->render('account/address_add.html.twig', [
+        return $this->render('account/address_form.html.twig', [
             'form' => $form->createView()
         ]);
     }
 
+    /**
+     * @Route("/compte/modifier-une-adresse/{id}", name="account_address_edit")
+     */
+    public function edit(Request $request, $id)
+    {
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        if (!$address || $address->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('account_address');
+        }
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/compte/supprimer-une-adresse/{id}", name="account_address_delete")
+     */
+    public function delete($id)
+    {
+        $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        if ($address && $address->getUser() == $this->getUser()) {
+            $this->entityManager->remove($address);
+            $this->entityManager->flush();
+        }
+            return $this->redirectToRoute('account_address');
+    }
 }
