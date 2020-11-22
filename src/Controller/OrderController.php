ba61096099ecce111f8867e9a9db8ad913303450
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-11-22 15:30:46 +0100

    Sommaire de la commande et ajout de style

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +14,7 @@ class OrderController extends AbstractController
     /**
      * @Route("/commande", name="order")
      */
-    public function index(Cart $cart)
+    public function index(Cart $cart, Request $request)
     {
         if (!$this->getUser()->getAddresses()->getValues())
         {
@@ -23,6 +24,12 @@ class OrderController extends AbstractController
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser() /* Me permettra de récupérer dans mon formulaire l'utilisateur en question pour récupérer ses adresses à lui et pas aussi celles des autres utilisateurs. | Voir ensuite OrderType.php */
         ]);
+
+            $form->handleRequest($request); /* Pour dire au formulaire : écoute la requête s'il te plait. */
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                //dd($form->getData());
+            }
 
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
