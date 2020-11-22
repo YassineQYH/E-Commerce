9f15b4cfa5e35f4a5ac747f670697da37543bc35
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-11-22 19:27:54 +0100

    Mapping des nouvelles entités avec EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,11 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Carrier;
 use App\Entity\Category;
+use App\Entity\Order;
 use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,8 +20,10 @@ class DashboardController extends AbstractDashboardController
      * @Route("/admin", name="admin")
      */
     public function index(): Response
-    {
-        return parent::index();
+    {        
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+
+        return $this->redirect($routeBuilder->setController(OrderCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -32,6 +36,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Orders', 'fa fa-shopping-cart', Order::class);
         yield MenuItem::linkToCrud('Catégories', 'fa fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fa fa-tag', Product::class);
         yield MenuItem::linkToCrud('Carriers', 'fa fa-truck', Carrier::class);
--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -99,7 +99,7 @@ class OrderController extends AbstractController
                     $this->entityManager->persist($orderDetails);
                 }
 
-                //$this->entityManager->flush();
+                $this->entityManager->flush();
 
                 return $this->render('order/add.html.twig', [
                     'cart' => $cart->getFull(),
