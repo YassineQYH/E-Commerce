5b8cbdfaa922c4648def3ae25ba1b1fef03a2b60
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-11-22 15:17:24 +0100

    Tunnel d'achat : Choix de l'adresse de livraison + Choix du transporteur avant de valider la commande
    Tunnel d'achat :
    ->Choix de l'adresse de livraison
    +
    ->Choix du transporteur avant de valider la commande

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use App\Form\OrderType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @Route("/commande", name="order")
+     */
+    public function index(Cart $cart)
+    {
+        if (!$this->getUser()->getAddresses()->getValues())
+        {
+            return $this->redirectToRoute('account_address_add');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser() /* Me permettra de récupérer dans mon formulaire l'utilisateur en question pour récupérer ses adresses à lui et pas aussi celles des autres utilisateurs. | Voir ensuite OrderType.php */
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
+        ]);
+    }
+}
