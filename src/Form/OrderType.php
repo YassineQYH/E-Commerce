6555ce50ec0533820db5394f513f4d14c7d67ab8
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-11-22 19:03:38 +0100

    Tunnel d'achat : stocker les informations de la commande en bdd

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -19,7 +19,7 @@ class OrderType extends AbstractType
 
         $builder
             ->add('addresses', EntityType::class, [
-                'label' => 'Choisissez votre adresse de livraison',
+                'label' => false,
                 'required' => true,
                 'class' => Address::class,
                 'choices' => $user->getAddresses(), /* Me permet de récupérer uniquement les adresses de l'utilisateur et non pas des autres */
